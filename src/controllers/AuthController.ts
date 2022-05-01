0a0fefff739b692ca875c38cf44596dddb251b17
Lucas Pevidor <lucas.pevidor@gmail.com>
2022-04-30 22:44:07 -0300

    Created AuthController file

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+import jsonwebtoken from 'jsonwebtoken';
+
+export default class AuthController {
+	public authenticate = async (
+		req: Request,
+		res: Response
+	): Promise<Response> => {
+		const prisma = new PrismaClient();
+		const { name, email } = req.body;
+		const userFound = await prisma.user.findFirst({
+			where: {
+				email
+			}
+		});
+
+		if (!userFound) {
+			return res.status(400).json({
+				error: 'User not found'
+			});
+		}
+
+		if (userFound.name !== name) {
+			return res.status(400).json({
+				error: 'User not found'
+			});
+		}
+
+		// Authenticate user with jsonWebToken
+		const token = jsonwebtoken.sign(
+			{
+				userId: userFound.id,
+				email: userFound.email,
+				name: userFound.name
+			},
+			process.env.JWT_SECRET,
+			{
+				expiresIn: '6h'
+			}
+		);
+
+		return res.json({
+			token,
+			user: {
+				id: userFound.id,
+				name: userFound.name,
+				email: userFound.email
+			}
+		});
+	};
+}
