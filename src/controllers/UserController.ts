47ac8c2d7f396eb982a5c42b514fd04e2e2ebb62
Lucas Pevidor <lucas.pevidor@gmail.com>
2022-04-30 21:59:21 -0300

    Implemented UserController

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+
+export default class UserController {
+	public createUser = async (
+		req: Request,
+		res: Response
+	): Promise<Response> => {
+		const prisma = new PrismaClient();
+
+		const { name, email } = req.body;
+
+		// check if email in use
+		const userFound = await prisma.user.findFirst({
+			where: {
+				email
+			}
+		});
+
+		if (userFound) {
+			return res.status(400).json({
+				error: 'Email already in use'
+			});
+		}
+
+		const user = await prisma.user.create({
+			data: {
+				name,
+				email
+			}
+		});
+
+		return res.json(user);
+	};
+}
